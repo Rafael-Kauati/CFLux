76280227ac0e644cc0895035ac0887d34a04a522
tk <kauati.toiti@gmail.com>
2024-08-11 16:15:03 +0100

    Sing in message

--- cflux/app/controllers/singin_controller.rb
+++ cflux/app/controllers/singin_controller.rb
@@ -10,10 +10,21 @@ class SinginController < ApplicationController
     end
 
     @user = User.new(user_params)
+
+    # Check if the email or name already exists in the database
+    if User.exists?(email: @user.email)
+      flash.now[:error] = "Email is already taken."
+      return render :new, status: :unprocessable_entity
+    elsif User.exists?(name: @user.name)
+      flash.now[:error] = "Username is already taken."
+      return render :new, status: :unprocessable_entity
+    end
+
     if @user.save
       UserMailer.welcome_email(@user).deliver_now
       redirect_to login_path
     else
+      flash.now[:error] = @user.errors.full_messages.join(', ')
       render :new, status: :unprocessable_entity
     end
   end
